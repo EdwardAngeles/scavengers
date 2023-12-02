790d1adf69752a65a79e691ada42de38d3dad796
Edward Angeles <edward.angelesm@gmail.com>
2023-12-02 23:29:13 +0000

    dev

--- scripts/board.gd
+++ scripts/board.gd
@@ -5,26 +5,33 @@ var viewport_width: int = ProjectSettings.get_setting("display/window/size/viewp
 var viewport_height: int = ProjectSettings.get_setting("display/window/size/viewport_height")
 
 const cell_size_width := 32
-const cell_size_height := 32
+const cell_size_height := 32 
 
-const scn_outler_walls = [
-	preload("res://nodes/outler_wall_0.tscn"),
-	preload("res://nodes/outler_wall_1.tscn")
-]
+@export var scn_outler_walls : Array[PackedScene] = []
+@export var scn_floors : Array[PackedScene] = []
 
 func _ready():
 	generate_outler_wall()
+	generate_floors()
 
 
 func _process(delta):
 	pass
 
 
-func generate_outler_wall():
+func generate_outler_wall() -> void:
 	for x in range(0, viewport_width, cell_size_width):
 		for y in range(0, viewport_height, cell_size_height):
 			if x == 0 or x == 288 or y == 0 or y == 288:
-				var outlet_wall_instance = Utils.choose(scn_outler_walls).instantiate()
-				outlet_wall_instance.position = Vector2(x, y)
-				add_child(outlet_wall_instance)
+				Utils.instance_on_parent(self, scn_outler_walls.pick_random(), Vector2(x, y))
+
+
+func generate_floors() -> void:
+	for x in range(cell_size_width, viewport_width - cell_size_width, cell_size_width):
+		for y in range(cell_size_height, viewport_height - cell_size_height, cell_size_height):
+			Utils.instance_on_parent(self, scn_floors.pick_random(), Vector2(x, y))
+
+
+func generate_inner_wall() -> void:
 	pass
+
--- scripts/utils.gd
+++ scripts/utils.gd
@@ -1,16 +1,20 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func choose(choises):
-	return choises[ randi_range(0, choises.size()-1) ]
-	pass
 
+func choose(choises):
+	return choises[randi_range(0, choises.size()-1)]
+
+func instance_on_parent(parent_node: Node, scene_to_instantiate: PackedScene, position: Vector2) -> Node:
+	var instance := scene_to_instantiate.instantiate()
+	if instance is Node2D:
+		instance.position = position
+	parent_node.add_child(instance)
+	return instance
